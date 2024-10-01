dd0cab15056ab991edaf25026d30c1476020f684
Tucker Mayfield <tuckmayf@gmail.com>
2024-10-01 19:51:48 -0400

    Created Main Application and created framework and migrations for SQL database

--- PokemonTCG/settings.py
+++ PokemonTCG/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pokemondb.apps.PokemondbConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
