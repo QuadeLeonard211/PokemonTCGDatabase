f3edd9e2730b99629e4673da13f829232155894f
Sam Lewis <samuellewis92@icloud.com>
2024-10-05 11:20:23 -0400

    Updated the homepage to be based on a html and css file rather than a html response

--- pokemondb/views.py
+++ pokemondb/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def pokemondb_homepage(request):
-    return HttpResponse("Pokemon TCG Database")
+    return render(request, 'pokemondb/homepage.html')
