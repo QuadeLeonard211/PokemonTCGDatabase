5b8e884d681a5117bd3ac0bc0877ed62e6d079c7
Quade Leonard <quade.leonard@yahoo.com>
2024-11-11 16:58:31 -0500

    added functions to add, remove and list current IDs of pokemon in specific users list

--- accounts/models.py
+++ accounts/models.py
@@ -5,5 +5,25 @@ class CustomUser(AbstractUser):
     pass
     listPokemon = models.JSONField(default=list, blank=True)
 
+    def add_pokemon(self, mon):
+        self.listPokemon.append(mon)
+        self.save()
+        print("Added " + mon)
+    
+    def remove_pokemon(self, mon):
+        if mon in self.listPokemon:
+            self.listPokemon.remove(mon)
+            self.save()
+            print("Removed " + mon)
+        else:
+            print(mon + " not in list")
+    
+    def get_pokemon(self):
+        return self.listPokemon
+    
+    def clear_pokemon(self):
+        self.listPokemon.clear()
+        self.save()
+
     def __str__(self):
         return self.username
