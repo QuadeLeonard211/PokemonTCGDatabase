f4eb35d8a96c02f52ab5e7013c49aa062f0c0634
Quade Leonard <quade.leonard@yahoo.com>
2024-11-19 14:34:22 -0500

    added missing sets to api call and added rarity to a saved value in the API_cards class

--- tcg_api/management/commands/fetch_cards.py
+++ tcg_api/management/commands/fetch_cards.py
@@ -6,23 +6,26 @@ class Command(BaseCommand):
     help = "Fetch data from API and populate database for SV6"
 
     def handle(self, *args, **kwargs):
-        response = requests.get('https://api.pokemontcg.io/v2/cards?q=set.id:sv6')
-        if response.status_code == 200:
-            pokemon_data = response.json()
-            for pokemon in pokemon_data['data']:
-                # self.stdout.write(f"Current Pokemon: {pokemon} (Type: {type(pokemon)})")
-                # if isinstance(pokemon, dict):
-                #     isbn = pokemon.get('id', 'N/A')  # Safely get 'isbn'
-                #     self.stdout.write(f"ID: {isbn}")
-                # else:
-                #     self.stderr.write("Unexpected data format. Skipping this entry.")
-                API_cards.objects.update_or_create(
-                    id=pokemon['id'],
-                    defaults={
-                        'name': pokemon['name'],
-                        'url': pokemon['tcgplayer']['url'],
-                    }
-                )
-            self.stdout.write(self.style.SUCCESS('Successfully populated the database'))
-        else:
-            self.stdout.write(self.style.ERROR('Failed to fetch data from the API'))
+        card_sets = {"sv1", "sv6", "sv7"}
+        for set in card_sets:
+            response = requests.get('https://api.pokemontcg.io/v2/cards?q=set.id:%s' % set)
+            if response.status_code == 200:
+                pokemon_data = response.json()
+                for pokemon in pokemon_data['data']:
+                    # self.stdout.write(f"Current Pokemon: {pokemon} (Type: {type(pokemon)})")
+                    # if isinstance(pokemon, dict):
+                    #     isbn = pokemon.get('id', 'N/A')  # Safely get 'isbn'
+                    #     self.stdout.write(f"ID: {isbn}")
+                    # else:
+                    #     self.stderr.write("Unexpected data format. Skipping this entry.")
+                    API_cards.objects.update_or_create(
+                        id=pokemon['id'],
+                        defaults={
+                            'name': pokemon['name'],
+                            'url': pokemon['tcgplayer']['url'],
+                            'rarity': pokemon['rarity'],
+                        }
+                    )
+                self.stdout.write(self.style.SUCCESS('Successfully populated the database with'))
+            else:
+                self.stdout.write(self.style.ERROR('Failed to fetch data from the API'))
--- tcg_api/models.py
+++ tcg_api/models.py
@@ -4,6 +4,7 @@ class API_cards(models.Model):
     id = models.CharField(max_length=255, unique=True, primary_key=True)
     name = models.CharField(max_length=255)
     url = models.CharField(max_length=255)
+    rarity = models.CharField(max_length=255, default="normal")
 
     def __str__(self):
         return self.id
