be8e3726b68dd1d540fc1b96257fdc3e4b8014f5
Quade Leonard <quade.leonard@yahoo.com>
2024-11-21 14:48:03 -0500

    added trending prices from cardmarket as a saved variable in the api class file

--- tcg_api/views.py
+++ tcg_api/views.py
@@ -4,6 +4,6 @@ from tcg_api.models import API_cards
 def card_list(request): 
     cards = API_cards.objects.all()
     for card in cards:
-        print(card.name)
+        print(card.name + " " + str(card.trendPrice))
     return render(request, 'home.html', {'cards': cards})
     
